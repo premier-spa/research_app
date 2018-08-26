73a6c4cdf3df07e11aa362102576aec110c19fbf
hiracky16 <h.piiice16@gmail.com>
2018-08-26 16:14:06 +0900

    ユーザ登録・編集・退会機能の作成 #2
    User モデルの作成（マイグレート済み）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#index'
   get 'homes/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
