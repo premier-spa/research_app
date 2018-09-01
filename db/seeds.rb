23108592baf3f68fd2efe7341e76e5bfc28c79ce
hiracky16 <h.piiice16@gmail.com>
2018-09-01 13:41:41 +0900

    categories に投入する seed のサンプル #3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+# カテゴリの設定
+puts "Insert Categories"
+CSV.foreach('public/categories.csv') do |row|
+  Category.create(:name => row[0], :description => row[1])
+end
