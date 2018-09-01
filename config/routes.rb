8404b1d2afb4a65b67352c258d3e1ae7f30af845
hiracky16 <h.piiice16@gmail.com>
2018-09-01 13:50:12 +0900

    モデルのアソシエーションと routes の設定の変更 #3

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'homes/show'
   resources :news
   resources :labs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :labs do
+    resources :news
+  end
 end
