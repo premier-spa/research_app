17c8670d074d532c63cbbbae19567c045749c13e
KENTO OKAMOTO <KENTO@KENTO-no-MacBook-Air.local>
2018-10-08 16:35:03 +0900

    #14 これで合ってるのかscaffoldしたworks

--- db/schema.rb
+++ db/schema.rb
@@ -103,6 +103,13 @@ ActiveRecord::Schema.define(version: 2018_10_06_033253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "works", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "albums", "labs"
   add_foreign_key "lab_users", "labs"
   add_foreign_key "lab_users", "users"
