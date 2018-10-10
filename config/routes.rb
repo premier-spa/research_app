a3b55c6c614502847b09153ca072c7da5038e4fa
hiracky16 <h.piiice16@gmail.com>
2018-10-10 21:46:52 +0900

    研究テーマ(work)の作成・更新・削除機能の実装 #14
    route の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  resources :works
   devise_for :users
   root to: 'homes#index'
   get 'homes/show'
-  resources :news
-  resources :labs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :labs do
     resources :news
-    resources :albums
+		resources :albums
+    resources :works
   end
 
 end
