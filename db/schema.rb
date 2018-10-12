673ccca4f3f21392ad44d2f4d68703acb81fa808
Kuratech5 <puyol5cu@gmail.com>
2018-10-12 23:54:18 +0900

    majorsテーブルとモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_033253) do
+ActiveRecord::Schema.define(version: 2018_10_12_141017) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,11 @@ ActiveRecord::Schema.define(version: 2018_10_06_033253) do
     t.string "access"
     t.float "lon"
     t.float "lat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "majors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
