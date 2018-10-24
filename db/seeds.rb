de05f3d28e6cc682d8a1c86461cb9f69699da915
Kuratech5 <puyol5cu@gmail.com>
2018-10-24 23:36:10 +0900

    読み込みを実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,31 @@ CSV.foreach('public/categories.csv') do |row|
   Category.create(:name => row[0], :description => row[1])
 end
 
-puts "Insert Majors"
+# 大学・大学院、学部・研究科、学科・専攻
+puts "Insert Universities, Courses and Majors"
+puts "Delete Universities, Courses and Majors"
+University.delete_all
+Course.delete_all
 Major.delete_all
-ActiveRecord::Base.connection.execute('ALTER TABLE majors AUTO_INCREMENT = 1')
-CSV.foreach('public/grad_univ.csv') do |row|
-  Major.create(:university => row[0], :url => row[1], :division => row[2], :address => row[3], :phone => row[4], :department => row[5], :course => row[6], :profession => row[7])
+puts "Insert Universities"
+CSV.foreach('public/univ_list.csv') do |row|
+  University.create(:id => row[0], :name => row[1])
 end
+
+puts "Insert Courses"
+CSV.foreach('public/univ_course_list.csv') do |row|
+  university = University.find_by(name: row[1])
+  Course.create(:id => row[0], :university_id => university.id, :name => row[2])
+end
+
+puts "Insert Majors"
+CSV.foreach('public/univ_course_major_list.csv') do |row|
+  university = University.find_by(name: row[1])
+  course = Course.find_by(name: row[2])
+  Major.create(:id => row[0], :university_id => university.id, :course_id => course.id, :name => row[3])
+end
+
+
 
 puts "Insert Prefectures and Cities"
 puts "Delete Prefectures and Cities"
