9d7c29b507bd987150543d1be784bde89cd65f4a
Eiji Kumakawa <morinokumakamesan@gmail.com>
2018-11-17 14:41:09 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,11 +24,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass #ここはコメントアウトだった
-  config.assets.enabled = true #ここはなかった
+  config.assets.css_compressor = :sass
+  config.assets.enabled = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true #ここはfalseだった
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
